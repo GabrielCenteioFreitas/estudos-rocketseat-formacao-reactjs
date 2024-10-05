90d34634e078eb85b731312719c475f9b921b7b6
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-10-05 19:34:29 -0300

    feat: add SignIn page

--- Chapter04/pizzashop/src/pages/_layouts/auth.tsx
+++ Chapter04/pizzashop/src/pages/_layouts/auth.tsx
@@ -8,13 +8,13 @@ export const AuthLayout = () => {
         h-full border-r border-r-foreground/5 bg-muted
         p-10 text-muted-foreground flex flex-col justify-between
       ">
-        <div className="flex items-center ga-3 text-lg font-medium text-foreground">
+        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
           <Pizza className="size-5" />
           <span className="font-semibold">pizza.shop</span>
         </div>
 
         <footer className="text-sm">
-          Painel do parceiro &coy; pizza.shop - {new Date().getFullYear()}
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
         </footer>
       </div>
 
