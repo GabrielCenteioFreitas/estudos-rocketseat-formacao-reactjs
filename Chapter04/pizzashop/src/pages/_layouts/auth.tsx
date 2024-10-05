8ca6719394e411d4dde921debf142eb280904aad
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-10-05 18:58:23 -0300

    feat: add auth layout

--- Chapter04/pizzashop/src/pages/_layouts/auth.tsx
+++ Chapter04/pizzashop/src/pages/_layouts/auth.tsx
@@ -1,11 +1,24 @@
 import { Outlet } from "react-router-dom";
+import { Pizza } from "lucide-react";
 
 export const AuthLayout = () => {
   return (
-    <div>
-      <h1>Authentication</h1>
+    <div className="min-h-screen grid grid-cols-2">
+      <div className="
+        h-full border-r border-r-foreground/5 bg-muted
+        p-10 text-muted-foreground flex flex-col justify-between
+      ">
+        <div className="flex items-center ga-3 text-lg font-medium text-foreground">
+          <Pizza className="size-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
 
-      <div>
+        <footer className="text-sm">
+          Painel do parceiro &coy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
