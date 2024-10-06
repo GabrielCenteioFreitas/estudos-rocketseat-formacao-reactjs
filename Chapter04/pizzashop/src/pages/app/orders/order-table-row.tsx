bf1aef0f29acf12b78ac37c38c5078329a69d939
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-10-06 17:06:21 -0300

    feat: add Orders page

---
+++ Chapter04/pizzashop/src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { ArrowRight, Search, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
+
+interface OrderTableRowProps {
+  
+}
+
+export const OrderTableRow = ({  }: OrderTableRowProps) => {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="size-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">3u892f3ek0j9803d90</TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell className="">
+        <div className="flex items-center gap-2">
+          <span className="size-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Diego Fernandes</TableCell>
+      <TableCell className="font-medium">R$ 149,90</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="size-3 mr-2" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="size-3 mr-2" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+}
