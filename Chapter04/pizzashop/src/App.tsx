71459d7df3880a006a2399e213c7bc5ed0a3d091
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-10-06 16:12:24 -0300

    feat: add Header component

--- Chapter04/pizzashop/src/App.tsx
+++ Chapter04/pizzashop/src/App.tsx
@@ -3,13 +3,16 @@ import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import './global.css'
 import { router } from './routes'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | Pizzashop' />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
+        <Helmet titleTemplate='%s | Pizzashop' />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
--- Chapter04/pizzashop/src/pages/_layouts/auth.tsx
+++ Chapter04/pizzashop/src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Pizza } from "lucide-react";
 
 export const AuthLayout = () => {
   return (
-    <div className="min-h-screen grid grid-cols-2">
+    <div className="min-h-screen grid grid-cols-2 antialiased">
       <div className="
         h-full border-r border-r-foreground/5 bg-muted
         p-10 text-muted-foreground flex flex-col justify-between
