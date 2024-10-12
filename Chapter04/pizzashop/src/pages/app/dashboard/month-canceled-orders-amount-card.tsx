c4e4296eef216bd91030484fa262a3b95ee4fb61
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-10-12 00:37:36 -0300

    feat: connect dashboard page with backend

--- Chapter04/pizzashop/src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
+++ Chapter04/pizzashop/src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -1,7 +1,14 @@
+import { getMonthCanceledOrdersAmount } from "@/api/get-month-canceled-orders-amount";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
 import { DollarSign } from "lucide-react";
 
 export const MonthCanceledOrdersAmountCard = () => {
+  const { data: monthCanceledOrdersAmount } = useQuery({
+    queryFn: getMonthCanceledOrdersAmount,
+    queryKey: ['metrics', 'month-canceled-orders-amount'],
+  })
+
   return (
     <Card>
       <CardHeader className="flex-row space-y-0 items-center justify-between pb-2">
@@ -11,12 +18,25 @@ export const MonthCanceledOrdersAmountCard = () => {
         <DollarSign className="size-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">
-          32
-        </span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-600">-2%</span> em relação ao mês passado
-        </p>
+        {monthCanceledOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthCanceledOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthCanceledOrdersAmount.diffFromLastMonth < 0 ? (
+                <span className="text-emerald-500 dark:text-emerald-400">
+                  {monthCanceledOrdersAmount.diffFromLastMonth}%
+                </span>
+              ) : (
+                <span className="text-rose-500 dark:text-rose-600">
+                  +{monthCanceledOrdersAmount.diffFromLastMonth}%
+                </span>
+              )}
+              {' '}em relação ao mês passado
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   );
--- Chapter04/pizzashop/src/pages/app/dashboard/revenue-chart.tsx
+++ Chapter04/pizzashop/src/pages/app/dashboard/revenue-chart.tsx
@@ -1,18 +1,37 @@
+import { getDailyRevenueInPeriod } from "@/api/get-daily-revenue-in-period";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { Label } from "@/components/ui/label";
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useMemo, useState } from "react";
+import { DateRange } from "react-day-picker";
 import { CartesianGrid, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 import colors from "tailwindcss/colors";
 
-const data = [
-  { date: '10/12', revenue: 1200 },
-  { date: '11/12', revenue: 800 },
-  { date: '12/12', revenue: 900 },
-  { date: '13/12', revenue: 400 },
-  { date: '14/12', revenue: 2300 },
-  { date: '15/12', revenue: 800 },
-  { date: '16/12', revenue: 640 },
-]
-
 export const RevenueChart = () => {
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
+  })
+
+  const { data: dailyRevenueInPeriod } = useQuery({
+    queryFn: () => getDailyRevenueInPeriod({
+      from: dateRange?.from,
+      to: dateRange?.to,
+    }),
+    queryKey: ['metrics', 'daily-revenue-in-period', dateRange],
+  })
+
+  const chartData = useMemo(() => {
+    return dailyRevenueInPeriod?.map((dailyRevenue) => {
+      return {
+        date: dailyRevenue.date,
+        receipt: dailyRevenue.receipt / 100,
+      }
+    })
+  }, [dailyRevenueInPeriod])
+
   return (
     <Card className="col-span-2">
       <CardHeader className="flex-row items-center justify-between pb-8">
@@ -24,43 +43,53 @@ export const RevenueChart = () => {
             Receita diária no período
           </CardDescription>
         </div>
+
+        <div className="flex items-center gap-2">
+          <Label>Período</Label>
+          <DateRangePicker
+            date={dateRange}
+            onDateChange={setDateRange}
+          />
+        </div>
       </CardHeader>
       <CardContent>
-        <ResponsiveContainer width="100%" height={240}>
-          <LineChart data={data} style={{ fontSize: 12 }}>
-            <XAxis
-              dataKey="date"
-              axisLine={false}
-              tickLine={false}
-              dy={16}
-            />
+        {dailyRevenueInPeriod && (
+          <ResponsiveContainer width="100%" height={240}>
+            <LineChart data={chartData} style={{ fontSize: 12 }}>
+              <XAxis
+                dataKey="date"
+                axisLine={false}
+                tickLine={false}
+                dy={16}
+              />
 
-            <YAxis
-              stroke="#888"
-              axisLine={false}
-              tickLine={false}
-              width={80}
-              tickFormatter={(value: number) =>
-                value.toLocaleString('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                })
-              }
-            />
+              <YAxis
+                stroke="#888"
+                axisLine={false}
+                tickLine={false}
+                width={80}
+                tickFormatter={(value: number) =>
+                  value.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })
+                }
+              />
 
-            <CartesianGrid
-              vertical={false}
-              className="stroke-muted"
-            />
+              <CartesianGrid
+                vertical={false}
+                className="stroke-muted"
+              />
 
-            <Line
-              type="linear"
-              strokeWidth={2}
-              dataKey="revenue"
-              stroke={colors.violet[500]}
-            />
-          </LineChart>
-        </ResponsiveContainer>
+              <Line
+                type="linear"
+                strokeWidth={2}
+                dataKey="receipt"
+                stroke={colors.violet[500]}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
       </CardContent>
     </Card>
   );
